319dc68e3c88ec1192bb5ce07ccf13bb41647ba3
zinilus123 <kailahbalqis5@gmail.com>
2026-07-22 15:36:37 +0800

    fix: variable i

--- studikasus7.js
+++ studikasus7.js
@@ -2,13 +2,13 @@ const nilai = [80, 95, 70, 88, 100, 65];
 let terbesar = nilai[0];
 let terkecil = nilai[0];
 
-for (let i = 1; i < nilai.length; i++) {
+for (let i = 0; i < nilai.length; i++) {
     if (nilai[i] > terbesar) {
         terbesar = nilai[i]
     }
 };
 
-for (let i = 1; i < nilai.length; i++) {
+for (let i = 0; i < nilai.length; i++) {
     if (nilai[i] < terkecil) {
         terkecil = nilai[i]
     }
